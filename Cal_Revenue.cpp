0c83d55ee7088c74c1fb3bc3b1f992d8f49a408f
Thanteeeeee <139093769+Thanteeeeee@users.noreply.github.com>
2023-07-17 11:27:33 +0700

    Add files via upload

---
+++ Cal_Revenue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string Name;
+	int year;
+	cout << "Enter name          : ";
+	cin  >> Name;
+	cout << "Enter Year of birth : ";
+	cin  >> year;
+	cout << "**********output*********\n"
+	cout << "Your Age : " << 2566-year << " years old" << endl;
+	system("pause");
+	return(0);
+}
+
